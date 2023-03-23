aa5196ea99862963416a56ea0738aa0ad6fd38b7
Pavan Adari <94852934+JustADude29@users.noreply.github.com>
2023-03-23 17:12:02 +0530

    overlays class

--- overlays.h
+++ overlays.h
@@ -4,28 +4,58 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+namespace overlays {
+    class button {
+    public:
+        button();
 
-class button{
-public:
-    button();
-    button(std::string text, sf::Color col, sf::Vector2i pos);
-    ~button();
+        button(std::string text, sf::Font fonts, sf::Color col, sf::Vector2f pos, float Textsize);
 
-    void setColor(sf::Color col){};
+        ~button();
 
+        void setSprite(sf::Image image1, sf::Image image2);
 
-private:
-    std::string content;
-    sf::Color baseCol;
-    sf::Vector2i position;
-};
+        void draw(sf::RenderTarget &target);
 
-button::button(std::string text, sf::Color col, sf::Vector2i pos) {
+    private:
+        std::string content;
+        sf::Color baseCol;
+        sf::Vector2f position;
+        sf::Font font;
+        float size;
+        float borderThickness;
+        sf::Text buttonText;
+
+        sf::Texture t1;
+        sf::Texture t2;
+        sf::Sprite buttonImage;
+    };
+}
+
+overlays::button::button(std::string text,sf::Font fonts, sf::Color col, sf::Vector2f pos, float Textsize) {
     content = text;
+    font = fonts;
     baseCol = col;
     position = pos;
+    size = Textsize;
 
+    borderThickness = 2;
 
+    buttonText.setString(content);
+    buttonText.setFont(font);
+
+    buttonImage.setPosition(position);
+}
+
+void overlays::button::setSprite(sf::Image image1, sf::Image image2) {
+    t1.loadFromImage(image1);
+    t2.loadFromImage(image2);
+    buttonImage.setTexture(t1);
+}
+
+void overlays::button::draw(sf::RenderTarget &target) {
+    target.draw(buttonImage);
+    target.draw(buttonText);
 }
 
 
